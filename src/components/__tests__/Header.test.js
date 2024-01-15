a231a9aff904a6673ec1eb27dca04e50e8ff79b3
Paresh Chawla <pareshchawla.56@gmail.com>
2024-01-15 12:44:07 +0530

    Test Header Component

---
+++ src/components/__tests__/Header.test.js
@@ -0,0 +1,71 @@
+import { fireEvent, render, screen } from "@testing-library/react";
+import Header from "../Header";
+import { Provider } from "react-redux";
+import { BrowserRouter } from "react-router-dom";
+import appStore from "../../utils/appStore";
+import "@testing-library/jest-dom";
+
+it("Should render Header component with a login button", () => {
+  render(
+    <BrowserRouter>
+      <Provider store={appStore}>
+        <Header />
+      </Provider>
+    </BrowserRouter>
+  );
+
+  const loginButton = screen.getByRole("button", { name: "Login" });
+
+  expect(loginButton).toBeInTheDocument();
+});
+
+it("Should render Header component with count of Cart item", () => {
+  render(
+    <BrowserRouter>
+      <Provider store={appStore}>
+        <Header />
+      </Provider>
+    </BrowserRouter>
+  );
+
+  const cartCount = screen.getByText("0");
+
+  expect(cartCount).toBeInTheDocument();
+});
+
+it("Should change login button to logout when clicked", () => {
+  render(
+    <BrowserRouter>
+      <Provider store={appStore}>
+        <Header />
+      </Provider>
+    </BrowserRouter>
+  );
+
+  const loginButton = screen.getByRole("button", { name: "Login" });
+
+  fireEvent.click(loginButton);
+
+  const logoutButton = screen.getByRole("button", { name: "Logout" });
+
+  expect(logoutButton).toBeInTheDocument();
+});
+
+it("Should appear dummy text when login button is clicked", () => {
+  render(
+    <BrowserRouter>
+      <Provider store={appStore}>
+        <Header />
+      </Provider>
+    </BrowserRouter>
+  );
+
+  const loginButton = screen.getByRole("button", { name: "Login" });
+
+  fireEvent.click(loginButton);
+
+  const nameText = screen.getByText(/dummy/);
+
+  expect(nameText).toBeInTheDocument();
+});
+
