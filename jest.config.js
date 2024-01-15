a6614f7442992fe5f5a13642c0ccdd8b16b7e88a
Paresh Chawla <pareshchawla.56@gmail.com>
2024-01-15 12:42:59 +0530

    disable img from jest

--- jest.config.js
+++ jest.config.js
@@ -181,6 +181,9 @@ const config = {
   //   "\\\\node_modules\\\\",
   //   "\\.pnp\\.[^\\\\]+$"
   // ],
+  moduleNameMapper: {
+    "\\.(png|jpg|jpeg|gif|svg)$": "<rootDir>/__mocks__/fileMock.js"
+  },
 
   // An array of regexp pattern strings that are matched against all modules before the module loader will automatically return a mock for them
   // unmockedModulePathPatterns: undefined,
