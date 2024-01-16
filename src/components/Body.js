27f7e652a3e01f5ad9246b0899efc97de37a3506
Paresh Chawla <pareshchawla.56@gmail.com>
2024-01-16 20:46:51 +0530

    Revert "Enable CORS"
    This reverts commit cc98dcdbc96743117ecf4b05ec4d145e25070eb8.

--- src/components/Body.js
+++ src/components/Body.js
@@ -20,7 +20,7 @@ const Body = () => {
   async function getRestaurants() {
     try {
       const response = await fetch(
-        "https://thingproxy.freeboard.io/fetch/https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.9351929&lng=77.62448069999999&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
+        "https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.9351929&lng=77.62448069999999&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
       );
       const jsonData = await response.json();
       const apiRestaurants =
