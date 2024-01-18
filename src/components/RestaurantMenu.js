28a68aad50014fc11da9e06934d82d24b89e74a0
Paresh Chawla <pareshchawla.56@gmail.com>
2024-01-18 11:15:06 +0530

    Revert "Cart margin set"
    This reverts commit 2b3d6f9fb1fba3f3b19d29c2fbec621ad64b55c4.

--- src/components/RestaurantMenu.js
+++ src/components/RestaurantMenu.js
@@ -1,13 +1,18 @@
-import { useState, useEffect } from "react";
-import RestaurantCategory from "./RestaurantCategory";
-import RestaurantInfo from "../../constants.js/resMenu.json";
+import { useState } from "react";
+import useRestaurantMenu from "../hooks/useRestaurantMenu";
 import ShimmerRestaurantMenu from "./ShimmerRestaurantMenu";
+import { useParams } from "react-router-dom";
+import RestaurantCategory from "./RestaurantCategory";
 
 const RestaurantMenu = () => {
   const [showIndex, setShowIndex] = useState(null);
-  const [showShimmer, setShowShimmer] = useState(true);
+  const { resId } = useParams();
 
-  const resInfo = RestaurantInfo.data;
+  const resInfo = useRestaurantMenu(resId);
+
+  if (resInfo === null) {
+    return <ShimmerRestaurantMenu />;
+  }
 
   const { name, cuisines, costForTwoMessage } =
     resInfo?.cards[0]?.card?.card?.info;
@@ -19,37 +24,26 @@ const RestaurantMenu = () => {
         "type.googleapis.com/swiggy.presentation.food.v2.ItemCategory"
     );
 
-  useEffect(() => {
-    const shimmerTimeout = setTimeout(() => {
-      setShowShimmer(false);
-    }, 2000);
-
-    // Cleanup timeout to avoid memory leaks
-    return () => clearTimeout(shimmerTimeout);
-  }, []); // Ensure useEffect runs only once on component mount
-
-  if (showShimmer) {
-    return <ShimmerRestaurantMenu />;
-  }
-
   return (
-    <div className="min-h-screen text-center my-6">
-      <h1 className="font-bold text-2xl">{name}</h1>
-      <h2 className="font-medium text-md my-3">
-        {cuisines.join(", ")} - {costForTwoMessage}
-      </h2>
-      {categories.map((category, index) => (
-        <RestaurantCategory
-          key={category?.card?.card?.title}
-          data={category?.card?.card}
-          setShowIndex={() => {
-            // Toggle the state explicitly based on the current showItems value
-            setShowIndex((prevIndex) => (prevIndex === index ? null : index));
-          }}
-          showItems={index === showIndex ? true : false}
-        />
-      ))}
-    </div>
+    <>
+      <div className="min-h-screen text-center my-6">
+        <h1 className="font-bold text-2xl">{name}</h1>
+        <h2 className="font-medium text-md my-3">
+          {cuisines.join(", ")} - {costForTwoMessage}
+        </h2>
+        {categories.map((category, index) => (
+          <RestaurantCategory
+            key={category?.card?.card?.title}
+            data={category?.card?.card}
+            setShowIndex={() => {
+              // Toggle the state explicitly based on the current showItems value
+              setShowIndex((prevIndex) => (prevIndex === index ? null : index));
+            }}
+            showItems={index === showIndex ? true : false}
+          />
+        ))}
+      </div>
+    </>
   );
 };
 
